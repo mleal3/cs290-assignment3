5d308fff4f10a8f085510570c7e285ba85eff1e6
Mitchel Leal <lealm@onid.oregonstate.edu>
2015-04-25 17:21:27 -0500

    Updated variables.js

--- src/variables.js
+++ src/variables.js
@@ -9,11 +9,18 @@ yourNumberIs: a string that says "Your Number is `x`." where `x` is replaced by
 in input `a`
 a: the original a number
 */
-function variableModification(a) {
+function variableModification(a) 
+{
   var plus5;
   var asString;
   var yourNumberIs;
   //your code here
+  
+  plus5 = a + 5;
+  
+  asString = a.toString();
+  
+  yourNumberIs = "Your Number is " + a;
 
   //end your code
   return [plus5, asString, yourNumberIs, a];
@@ -27,9 +34,17 @@ Output:
 return true if b is a primitive string value (also known as a string literal),
 false otherwise
 */
-function isString(b) {
+function isString(b) 
+{
   //your code here
-  return undefined;
+  if (typeof b === "string")
+  {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
   //end your code
 }
 
@@ -40,8 +55,18 @@ c: could be anything
 Output:
 return true if c is null, false otherwise
 */
-function isNull(c) {
+function isNull(c) 
+{
   //your code here
-  return undefined;
+  
+  if (c === null)
+  {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+  
   //end your code
 }
